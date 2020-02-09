baa472e442dae244fe759ddab21262ab08110909
vselfa <vselfa@ieseljust.com>
2020-02-09 21:26:10 +0100

    First commit of Vulcano

---
+++ Assets/Scripts/Eruption.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eruption : MonoBehaviour {
+    public GameObject stone;
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f; // Initial value
+    // Use this for initialization
+    void Start () { 	}
+    // Update is called once per frame
+    void Update () {
+	    if (Time.time > nextFire)   {
+            nextFire = Time.time + fireRate;
+            // Object, position and rotation of the new fire
+            Instantiate(stone, transform.position, Random.rotation);
+           } 
+    }
+}
---
+++ Assets/Scripts/Impulse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulse : MonoBehaviour {
+    private Rigidbody rigidBody;
+    public float forceImpulse = 1.0f;
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        // The initial impulse to be applied to every stone only 	// when they are created
+        rigidBody.AddForce(Vector3.up * forceImpulse,  ForceMode.Impulse);
+    }
+    // Update is called once per frame
+    void Update () {	}
+}
