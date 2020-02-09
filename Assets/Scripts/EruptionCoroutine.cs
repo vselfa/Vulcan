f4c6c8b04c5ad6f61abaf17acf8841b7def3f4a8
vselfa <vselfa@ieseljust.com>
2020-02-09 21:46:44 +0100

    Vulcano with Coroutines

---
+++ Assets/Scripts/EruptionCoroutine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EruptionCoroutine : MonoBehaviour {
+    public GameObject stone;      
+    public float fireRate = 0.5f;
+    // Use this for initialization
+    void Start () { // Registering the coroutine
+        StartCoroutine(ThrowStone());
+     }
+    // The method for the coroutine
+    IEnumerator ThrowStone ()  {
+        // Star the coroutine after 2 seconds
+        yield return new WaitForSeconds(2);
+        while (true)  {
+            Instantiate(stone, transform.position, Random.rotation);
+            // To call the coroutine after a period of time
+            yield return new WaitForSeconds (fireRate);
+        }
+    }
+}
+
